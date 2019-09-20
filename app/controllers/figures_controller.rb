d878549efa1c808135cea87707787144e1471893
Tracy Reuther <tracyreuther@gmail.com>
2019-09-20 20:03:40 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,7 @@
 class FiguresController < ApplicationController
   # add controller methods
 end
+#This is where the magic happens
+#When a user creates or edits a figure, they should also be able to select or create a new landmark and/or title to associate to that figure.
+#The form for a new figure and the form to edit a figure should allow the user to select from existing landmarks and title and create new landmarks and titles to associate to the figure.
+#New titles will only get created in the context of creating or editing a figure.
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,6 @@
 class LandmarksController < ApplicationController
   # add controller methods
 end
+
+#View pages for all LandmarksController
+#Create new landmarks
