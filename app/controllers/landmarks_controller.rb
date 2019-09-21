1bbcd444d0c498400d98d304061d7a6572218ac8
Tracy Reuther <tracyreuther@gmail.com>
2019-09-21 14:45:53 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -12,6 +12,7 @@ class LandmarksController < ApplicationController
 
   post '/landmarks' do
     @landmark = Landmark.create(params["landmark"])
+    erb :'/landmarks/#{@landmark.id}'
   end
 
 end
