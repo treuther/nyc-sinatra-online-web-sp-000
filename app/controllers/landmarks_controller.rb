533d16c5ba5ce05f50eb2601ebf626b69f9a672a
Tracy Reuther <tracyreuther@gmail.com>
2019-09-21 14:37:19 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -4,6 +4,11 @@ class LandmarksController < ApplicationController
     @landmarks = Landmark.all
     erb :'/landmarks/index'
   end
+
+  get '/landmarks/new' do
+
+    erb :'/landmarks/new'
+  end
 end
 
 #View pages for all LandmarksController
