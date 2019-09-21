b4b4140a390a1d409589912db3f3b469a6997850
Tracy Reuther <tracyreuther@gmail.com>
2019-09-21 14:33:48 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -40,6 +40,8 @@ class FiguresController < ApplicationController
 
   get '/figures/:id/edit' do
     @figure = Figure.find(params[:id])
+    @landmarks = Landmark.all
+    @titles = Title.all
     erb :'figures/edit'
   end
 
